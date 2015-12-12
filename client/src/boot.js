c661f5cb5f31dcbe29e72daa310dec1408b55b41
ysw <yswai1986@gmail.com>
2015-12-12 23:03:12 +0800

    edit

--- client/src/boot.js
+++ client/src/boot.js
@@ -2,15 +2,16 @@ import 'angular-material/angular-material.css!'
 
 import _ from 'lodash'
 import $ from 'jquery'
-import myFunc from './my-func.js'
+import { myFunc1, myFunc2 } from './my-func.js'
 import angular from 'angular'
 import ngMaterial from 'angular-material'
 import mainModule from './main.js'
 
-console.log( ('Booting angular ... ' + angular ? 'OK' : 'FAILED') );
-console.log( ('Booting lodash ... ' + _ ? 'OK' : 'FAILED') );
-console.log( ('Booting jquery ... ' + $ ? 'OK' : 'FAILED') );
-console.log( ('Booting myFunc ... ' + myFunc ? 'OK' : 'FAILED') );
+console.log( 'Booting angular ... ' + (angular ? 'OK' : 'FAILED') );
+console.log( 'Booting lodash ... ' + ( _ ? 'OK' : 'FAILED') );
+console.log( 'Booting jquery ... ' + ( $ ? 'OK' : 'FAILED') );
+console.log( 'Booting myFunc1 ... ' + ( myFunc1 ? 'OK' : 'FAILED') );
+console.log( 'Booting myFunc2 ... ' + ( myFunc2 ? 'OK' : 'FAILED') );
 
 angular.element(document).ready(function () {
     let app = angular.module('app', [ngMaterial, mainModule.name]);
--- client/src/my-func.js
+++ client/src/my-func.js
@@ -1,8 +1,12 @@
 let myFunc = function(name) {
     return {
         s: name,
-        l: name + ' sohai'
+        l: name + ' asdf'
     };
+};
+
+let myFunc2 = function() {
+    return 'Func2';
 };
 
 let {s, l} = myFunc('IPG');
@@ -10,4 +14,7 @@ let {s, l} = myFunc('IPG');
 console.log('s = ' + s);
 console.log('l = ' + l);
 
-export default myFunc;
+export default {
+    myFunc1: myFunc,
+    myFunc2: myFunc2
+};
